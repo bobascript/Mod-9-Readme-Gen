86b842d7729a186d7a9976adba299f6f94e63313
mintghostii <alvarezjavi960@gmail.com>
2023-02-28 19:39:26 -0500

    Completed generateMarkdown file and added the packages and questions to the index.js file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,45 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+${data.license}
+## Description
+${data.description}
+## Table of Contents
+**[Installation](#Installation)**<br>
+**[Usage](#Usage)**<br>
+**[License](#License)**<br>
+**[Contributing](#Contributing)**<br>
+**[Tests](#Tests)**<br>
+**[Questions](#Questions)**<br>
+**[Video](#Video)**<br>
+## Installation
+${data.install} 
+## Usage
+${data.usage}
+## License
+This project is licensed under the ${license} license.
+## Contributing
+${data.contribution}
+## Questions
+If you have any questions about this project, open an issue or contact me directly at 
+${
+  data.email
+}. If you want to take a look at more of my work click on the following link:
+https://github.com/${
+    data.username
+  }. Thank you for your time and I hope you enjoyed.
+## Video
+For a walkthrough video click on the following link:
+
 
 `;
 }
